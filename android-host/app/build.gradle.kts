82502393cc5128549f1c8e4aed70726d59a2526c
Nick Valente <nick.valente@oakknoll.org>
2026-03-15 12:38:58 -0400

    Add implementation for Material Components library

--- android-host/app/build.gradle.kts
+++ android-host/app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     implementation(composeBom)
